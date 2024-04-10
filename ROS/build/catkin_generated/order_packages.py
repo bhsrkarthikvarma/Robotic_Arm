e409b54fa9a599533b684920c3fa0d0616c88e23
bhsrkarthikvarma <bhkv14@gmail.com>
2024-04-10 11:09:34 +0530

    Added ROS workspace.

---
+++ ROS/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/sunny/Desktop/3DOF_Robotic_Arm/ROS/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/sunny/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/sunny/catkin_ws/devel;/opt/ros/noetic' != '' else []
---
+++ ROS/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/sunny/Desktop/3DOF_Robotic_Arm/ROS/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/noetic/bin:/home/sunny/miniconda3/condabin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PWD='/home/sunny/Desktop/3DOF_Robotic_Arm/ROS/build'
+export ROS_PACKAGE_PATH="/home/sunny/Desktop/3DOF_Robotic_Arm/ROS/src:$ROS_PACKAGE_PATH"
